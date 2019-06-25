ff3a6531183fd86d6890560e7edf01b53afe16c2
Nitheesh Prabu <nitheeshprabu.s@gmail.com>
2019-06-25 18:55:59 +0530

    Added child routing, fixed bugs

--- routing/src/app/app-routing.module.ts
+++ routing/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { UserComponent } from './users/user/user.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'users', component: UsersComponent, children: [
+    { path: ':id/:name', component: UserComponent }, // dynamic route
+  ]},
+  { path: 'servers', component: ServersComponent, children: [
+    { path: ':id', component: ServerComponent },
+    { path: ':id/edit', component: EditServerComponent }
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- routing/src/app/users/user/user.component.ts
+++ routing/src/app/users/user/user.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params.id,
+      name: this.route.snapshot.params.name
+    };
+
+    // params is an observable
+    // use this to make sure the component gets informed about changes in the route params
+    this.paramsSubscription = this.route.params.subscribe(
+      (params: Params) => { this.user.id = params.id; this.user.name = params.name; }
+    );
   }
 
+  ngOnDestroy() {
+    // Angular would auto do this for Route Observables, but for other Observables we
+    // have to manually unsubscribe
+    this.paramsSubscription.unsubscribe();
+  }
 }
